93c4791d280f351dced5ce161064cff286249d5a
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-03-24 18:17:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -184,7 +184,7 @@ end
 def big_shoe_rebounds
   game_hash.each do |location, players |
     players[:players].each do |player|
-      if player[:rebounds] == name
+      if player[:rebounds].max
         return player[:shoe]
       end
     end
